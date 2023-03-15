0e76aafa4caaa42bba538b6c9609da61b380e848
Ozelot <29740364+0zelot@users.noreply.github.com>
2023-03-15 20:35:31 +0100

    Some fixes

--- main.py
+++ main.py
@@ -14,13 +14,16 @@ for item in config["followed"]:
     results = fetch(item)
 
     for new in results:
-        if(new not in data[results[0]["author"]["id"]]): 
-            push(new)
+        if(new["author"]["id"] in data and new["video"]["id"] not in data[new["author"]["id"]]): 
+            for subscriber in item["subscriptions"]:
+                for targetedSubscriber in config["subscribers"]:
+                    if(targetedSubscriber["name"] == subscriber):
+                        push(targetedSubscriber["url"], new)
 
-    data[results[0]["author"]["id"]] = results
+    data[item["id"]] = [item["video"]["id"] for item in results]
 
-    with open("./data.json", "w") as f:
-        json.dump(data, f, indent=4)
+    with open("./data.json", "w") as file:
+        json.dump(data, file, indent=4)
 
 file_config.close()
 file_data.close()
--- utils/push.py
+++ utils/push.py
@@ -1,2 +1,10 @@
-def push(item):
-    print("Pushing...", item["video"]["title"])
+import requests
+
+def push(url, item):
+    requests.post(url,
+        data = "{} posted a new video: {}".format(item["author"]["name"], item["video"]["title"]).encode("utf-8"),
+        headers = {
+            "Title": item["video"]["title"].encode("utf-8"),
+            "Icon": item["video"]["thumbnail"],
+            "Click": item["video"]["url"]
+        })
